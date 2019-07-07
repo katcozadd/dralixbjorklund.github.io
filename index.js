a97d1cfaf1a8923e372b32017e4be94ba07cd3e5
katcozadd <krosecozadd@gmail.com>
2019-07-06 22:47:29 -0600

    lots and lots of progress

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("sanity check");
+
+// Initialize and add the map
+function initMap() {
+  // The location of the cafe
+  var office = {lat: 35.656440, lng: -105.951910};
+  // The map, centered at the cafe
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 16, center: office});
+  // The marker, positioned at the cafe
+  var marker = new google.maps.Marker({position: office, map: map});
+}
+ 
